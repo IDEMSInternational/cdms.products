b43756dc8a4721b56666d07e03650b83679b48f7
lilyclements <lclements1004@outlook.com>
2022-02-11 14:42:49 +0000

    example edit in inventory_table

--- R/inventory_table.R
+++ R/inventory_table.R
@@ -15,6 +15,7 @@
 #' @export
 #'
 #' @examples # TODO
+#' data(niger_daily_date)
 #' inventory_table(data = niger_daily_date, station = "Name", date = "date", elements = "tmax")
 inventory_table <- function(data, date, elements, station = NULL, year = NULL, month = NULL,  
                             day = NULL, missing_indicator = "M", observed_indicator = "X") {
