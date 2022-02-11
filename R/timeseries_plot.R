e2712b6eb5dc19bef1374411720d258eb1023ef8
lilyclements <lclements1004@outlook.com>
2022-02-11 14:23:29 +0000

    timeseries_plot

--- R/timeseries_plot.R
+++ R/timeseries_plot.R
@@ -11,8 +11,8 @@
 #' @param se logical. If \code{TRUE}, the standard error is are added to the line of best fit. Only works if \code{add_line_of_best_fit = TRUE}. 
 #' @param add_path logical. If \code{TRUE}, paths are added to the plot using  \code{"ggplot2::geom_path()"}.
 #' @param add_step logical. If \code{TRUE}, steps are added to the plot using  \code{"ggplot2::geom_step()"}.
-#' @param na.rm If \code{FALSE}, the default, missing values are removed with a warning. If \code{TRUE}, missing values are silently removed.
-#' @param show.legend logical. Should this layer be included in the legends? \code{NA}, the default, includes if any aesthetics are mapped. \code{FALSE} never includes, and \code{TRUE} always includes.
+#' @param na_rm If \code{FALSE}, the default, missing values are removed with a warning. If \code{TRUE}, missing values are silently removed.
+#' @param show_legend logical. Should this layer be included in the legends? \code{NA}, the default, includes if any aesthetics are mapped. \code{FALSE} never includes, and \code{TRUE} always includes.
 #'
 #' @return
 #' @export
@@ -21,7 +21,7 @@
 timeseries_plot <- function(data, date_time, elements, station = NULL, facets = c("stations", "elements", "both", "none"),
                             add_points = FALSE, add_line_of_best_fit = FALSE,
                             se = TRUE, add_path = FALSE, add_step = FALSE,
-                            na.rm = FALSE, show.legend = NA){
+                            na_rm = FALSE, show_legend = NA){
   
   checkmate::assert_data_frame(data)
   # date_time can be a date, factor, or character.
@@ -33,7 +33,8 @@ timeseries_plot <- function(data, date_time, elements, station = NULL, facets = 
   checkmate::assert_logical(se)
   checkmate::assert_logical(add_path)
   checkmate::assert_logical(add_step)
-  checkmate::assert_logical(show.legend)
+  checkmate::assert_logical(na_rm)
+  checkmate::assert_logical(show_legend)
   
   if ((facets == "stations" || facets == "both") && is.null(station)) {
     warning("facets will be set to 'none' since station is missing")
@@ -78,7 +79,7 @@ timeseries_plot <- function(data, date_time, elements, station = NULL, facets = 
     }
   }
   
-  base_plot <- base_plot + ggplot2::geom_line(na.rm = na.rm, show.legend = show.legend)
+  base_plot <- base_plot + ggplot2::geom_line(na.rm = na_rm, show.legend = show_legend)
   
   # color by viridis?
   # base_plot <- base_plot + viridis::scale_colour_viridis(discrete = TRUE, option = "C") # colour blind friendly
