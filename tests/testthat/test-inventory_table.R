012438ec1154785c5dd71f0203806b19acd4a61b
Haward Ketoyo <hketoyo@gmail.com>
2022-02-27 20:49:33 +0300

    updated the code
    Added two for loops to reduce the lines of code.

--- tests/testthat/test-inventory_table.R
+++ tests/testthat/test-inventory_table.R
@@ -1,88 +1,69 @@
 library(RInstatClimatic)
 data("daily_niger")
 all_elements <- c("tmax","tmin","rain","hmax","hmin","sunh","ws","wd")
+list_of_days <- c("day","doy")
 
 #Functions output for one element (rain) for day
-x1_day_inventory <- inventory_table(data = daily_niger, date = "date", elements = c("rain"), 
-                                    station = "station_name", year = "year", month = "month", 
-                                    day = "day")
+x_day_rain <- inventory_table(data = daily_niger, date = "date", elements = c("rain"), 
+                              station = "station_name", year = "year", month = "month", 
+                              day = "day")
 #Functions output for all elements for day
-x2_day_inventory <- inventory_table(data = daily_niger, date = "date", 
-                                    elements = all_elements, 
-                                    station = "station_name", year = "year", month = "month", 
-                                    day = "day")
+x_day_all_elements <- inventory_table(data = daily_niger, date = "date", 
+                                      elements = all_elements, 
+                                      station = "station_name", year = "year", month = "month", 
+                                      day = "day")
 #Functions output for one element (rain) for doy
-x1_doy_inventory <- inventory_table(data = daily_niger, date = "date", elements = c("rain"), 
-                                    station = "station_name", year = "year", month = "month", 
-                                    day = "doy")
+x_doy_rain <- inventory_table(data = daily_niger, date = "date", elements = c("rain"), 
+                              station = "station_name", year = "year", month = "month", 
+                              day = "doy")
 #Functions output for all elements for doy
-x2_doy_inventory <- inventory_table(data = daily_niger, date = "date", 
-                                    elements = all_elements, 
-                                    station = "station_name", year = "year", month = "month", 
-                                    day = "doy")
+x_doy_all_elements <- inventory_table(data = daily_niger, date = "date", 
+                                      elements = all_elements, 
+                                      station = "station_name", year = "year", month = "month", 
+                                      day = "doy")
 
 #Expected output for one element (rain) for day
-y1_day_inventory_data <- daily_niger %>%
-  tidyr::pivot_longer(cols = tidyselect::all_of(c("rain")), names_to = "element") %>%
-  dplyr::select(c(tidyselect::all_of(c("station_name", "year", "month")), day, element, value)) %>%
-  dplyr::mutate(value = ifelse(is.na(value), "M", "X"))
-y1_day_summary_data <- y1_day_inventory_data %>%
-  dplyr::group_by(station_name, element, year, month)  %>%
-  dplyr::summarise(Available = sum(value == "X"), Missing = sum(value == "M"))
-y1_day_inventory_data_wider <- y1_day_inventory_data %>%
-  tidyr::pivot_wider(id_cols = c(tidyselect::all_of(c("station_name", "year", "month")), element),
-                     names_from = day, values_from = value)
-y1_day_inventory <- dplyr::full_join(y1_day_inventory_data_wider, y1_day_summary_data)
+for (i in list_of_days) {
+  inventory_data <- daily_niger %>%
+    tidyr::pivot_longer(cols = tidyselect::all_of(c("rain")), names_to = "element") %>%
+    dplyr::select(c(tidyselect::all_of(c("station_name", "year", "month")), i, element, value)) %>%
+    dplyr::mutate(value = ifelse(is.na(value), "M", "X"))
+  summary_data <- inventory_data %>%
+    dplyr::group_by(station_name, element, year, month)  %>%
+    dplyr::summarise(Available = sum(value == "X"), Missing = sum(value == "M"))
+  inventory_data_wider <- inventory_data %>%
+    tidyr::pivot_wider(id_cols = c(tidyselect::all_of(c("station_name", "year", "month")), element),
+                       names_from = i, values_from = value)
+  inventory <- dplyr::full_join(inventory_data_wider, summary_data)
+  assign(paste0("y_", i ,"_rain"),inventory)
+}
 
-#Expected output for all elements for doy
-y2_day_inventory_data <- daily_niger %>%
-  tidyr::pivot_longer(cols = tidyselect::all_of(all_elements), names_to = "element") %>%
-  dplyr::select(c(tidyselect::all_of(c("station_name", "year", "month")), day, element, value)) %>%
-  dplyr::mutate(value = ifelse(is.na(value), "M", "X"))
-y2_day_summary_data <- y2_day_inventory_data %>%
-  dplyr::group_by(station_name, element, year, month)  %>%
-  dplyr::summarise(Available = sum(value == "X"), Missing = sum(value == "M"))
-y2_day_inventory_data_wider <- y2_day_inventory_data %>%
-  tidyr::pivot_wider(id_cols = c(tidyselect::all_of(c("station_name", "year", "month")), element),
-                     names_from = day, values_from = value)
-y2_day_inventory <- dplyr::full_join(y2_day_inventory_data_wider, y2_day_summary_data)
-
-
-#Expected output for one element (rain) for doy
-y1_doy_inventory_data <- daily_niger %>%
-  tidyr::pivot_longer(cols = tidyselect::all_of(c("rain")), names_to = "element") %>%
-  dplyr::select(c(tidyselect::all_of(c("station_name", "year", "month")), day, element, value)) %>%
-  dplyr::mutate(value = ifelse(is.na(value), "M", "X"))
-y1_doy_summary_data <- y1_doy_inventory_data %>%
-  dplyr::group_by(station_name, element, year, month)  %>%
-  dplyr::summarise(Available = sum(value == "X"), Missing = sum(value == "M"))
-y1_doy_inventory_data_wider <- y1_doy_inventory_data %>%
-  tidyr::pivot_wider(id_cols = c(tidyselect::all_of(c("station_name", "year", "month")), element),
-                     names_from = day, values_from = value)
-y1_doy_inventory <- dplyr::full_join(y1_doy_inventory_data_wider, y1_doy_summary_data)
-
-#Expected output for all elements for doy
-y2_doy_inventory_data <- daily_niger %>%
-  tidyr::pivot_longer(cols = tidyselect::all_of(all_elements), names_to = "element") %>%
-  dplyr::select(c(tidyselect::all_of(c("station_name", "year", "month")), day, element, value)) %>%
-  dplyr::mutate(value = ifelse(is.na(value), "M", "X"))
-y2_doy_summary_data <- y2_doy_inventory_data %>%
-  dplyr::group_by(station_name, element, year, month)  %>%
-  dplyr::summarise(Available = sum(value == "X"), Missing = sum(value == "M"))
-y2_doy_inventory_data_wider <- y2_doy_inventory_data %>%
-  tidyr::pivot_wider(id_cols = c(tidyselect::all_of(c("station_name", "year", "month")), element),
-                     names_from = day, values_from = value)
-y2_doy_inventory <- dplyr::full_join(y2_doy_inventory_data_wider, y2_doy_summary_data)
+#Expected output for one element (rain) for day
+for (i in list_of_days) {
+  inventory_data <- daily_niger %>%
+    tidyr::pivot_longer(cols = tidyselect::all_of(c("tmax","tmin","rain","hmax","hmin","sunh","ws","wd")), names_to = "element") %>%
+    dplyr::select(c(tidyselect::all_of(c("station_name", "year", "month")), i, element, value)) %>%
+    dplyr::mutate(value = ifelse(is.na(value), "M", "X"))
+  summary_data <- inventory_data %>%
+    dplyr::group_by(station_name, element, year, month)  %>%
+    dplyr::summarise(Available = sum(value == "X"), Missing = sum(value == "M"))
+  inventory_data_wider <- inventory_data %>%
+    tidyr::pivot_wider(id_cols = c(tidyselect::all_of(c("station_name", "year", "month")), element),
+                       names_from = i, values_from = value)
+  inventory <- dplyr::full_join(inventory_data_wider, summary_data)
+  assign(paste0("y_",i,"_all_elements"),inventory)
+}
 
 test_that("inventory_table returns correct length", {
-  expect_length(x1_day_inventory, 37)
-  expect_length(x2_day_inventory, 37)
-  expect_length(x1_doy_inventory, 372)
-  expect_length(x2_doy_inventory, 372)
+  expect_length(x_day_rain, 37)
+  expect_length(x_doy_rain, 372)
+  expect_length(x_day_all_elements, 37)
+  expect_length(x_doy_all_elements, 372)
 })
+
 test_that("inventory_table returns correct result", {
-  expect_equal(x1_day_inventory,y1_day_inventory)
-  expect_equal(x2_day_inventory,y2_day_inventory)
-  expect_equal(x1_doy_inventory,y1_doy_inventory)
-  expect_equal(x2_doy_inventory,y2_doy_inventory)
+  expect_equal(x_day_rain,y_day_rain)
+  expect_equal(x_doy_rain,y_doy_rain)
+  expect_equal(x_day_all_elements,y_day_all_elements)
+  expect_equal(x_doy_all_elements,y_doy_all_elements)
 })
