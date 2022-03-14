88be9b45535de4b7edc6c65732f4c9bbf9ca7d6b
Haward Ketoyo <hketoyo@gmail.com>
2022-03-14 13:19:41 +0300

    Update test-prepare_cdt_dekad
    Added a test to check whether prepare_cdt_dekad returns an error when conditions are not met

--- tests/testthat/test-prepare_cdt_dekad.R
+++ tests/testthat/test-prepare_cdt_dekad.R
@@ -1,3 +1,4 @@
+library(cdms.products)
 library(dplyr)
 library(tidyr)
 
@@ -29,10 +30,26 @@ x_dekad <- x_dekad %>%
   tidyr::pivot_wider(names_from = station_name, values_from = values)
 
 #  create functions dekad output
-y_dekad <- prepare_cdt(data = dekad_summary_data, date_time = "date",
+y_dekad <- prepare_cdt_dekad(data = dekad_summary_data, date_time = "date",
                        station = "station_name", element = "sum", metadata = stations_niger, 
                        latitude = "lat", longitude = "long", altitude = "alt")
 
-test_that("prepare-cdt gives correct values", {
+test_that("prepare_cdt gives correct values", {
   expect_equal(x_dekad,y_dekad)
+})
+
+test_that("prepare_cdt_dekad returns an error when conditions are not met", {
+  mandatory_inputs <- list(data = dekad_summary_data, date_time = "date",
+                           station = "station_name", element = "sum", metadata = stations_niger,
+                           latitude = "lat", longitude = "long", altitude = "alt")
+  
+  expect_error(prepare_cdt_dekad(),)
+  expect_error(do.call(prepare_cdt_dekad, mandatory_inputs[-1]))
+  expect_error(do.call(prepare_cdt_dekad, mandatory_inputs[-2]))
+  expect_error(do.call(prepare_cdt_dekad, mandatory_inputs[-3]))
+  expect_error(do.call(prepare_cdt_dekad, mandatory_inputs[-4]))
+  expect_error(do.call(prepare_cdt_dekad, mandatory_inputs[-5]))
+  expect_error(do.call(prepare_cdt_dekad, mandatory_inputs[-6]))
+  expect_error(do.call(prepare_cdt_dekad, mandatory_inputs[-7]))
+  expect_error(do.call(prepare_cdt_dekad, mandatory_inputs[-8]))
 })
