50150d8773f8de652a128d7d4af0c16863cdd9fc
lilyclements <lclements1004@outlook.com>
2022-03-28 18:04:59 +0100

    documentation for climatic_missing

--- R/climatic_missing.R
+++ R/climatic_missing.R
@@ -1,85 +1,93 @@
-#' Title
+#' Summarise missing data in a data frame
 #'
 #' @param data The data.frame to calculate from.
 #' @param date_time The name of the date column in \code{data}.
 #' @param elements The name of the column in \code{data} to apply the function to.
-#' @param stations The name of the station column in \code{data}, if the data are for multiple station. 
+#' @param station_id The name of the station column in \code{data}, if the data are for multiple station. 
 #' The calculations are performed separately for each station.
 #' @param start \code{logical(1)} If \code{TRUE} start date as ...
 #' @param end \code{logical(1)} If \code{TRUE} set end date as ...
 #'
-#' @return
+#' @return Data frame summarising the missing data
 #' @export
 #'
-#' @examples # TODO
-climatic_missing <- function(data, date_time, elements, stations,
+#' @examples 
+#' # Summarising missing data in the rainfall, temperature, and sun columns
+#' climatic_missing(data = daily_niger, date_time = "date", 
+#'                  elements = c("rain", "tmax", "tmin", "sunh"),
+#'                  station_id = "station_name")
+#' 
+climatic_missing <- function(data, date_time, elements, station_id = NULL,
                              start = TRUE, end = FALSE) {
+  checkmate::assert_data_frame(data)
+  assert_column_names(data, date_time)
+  checkmate::assert_string(date_time)
+  checkmate::assert(checkmate::check_date(data[[date_time]]), 
+                    checkmate::check_posixct(data[[date_time]]))
+  checkmate::assert_character(elements)
+  assert_column_names(data, elements)
+  if (!is.null(station_id)) assert_column_names(data, station_id)
+  if (!is.null(station_id)) checkmate::assert_character(station_id)
+  checkmate::assert_logical(start)
+  checkmate::assert_logical(end)
   
-  if (missing(date_time)) {
-    stop('argument "date_time" is missing, with no default')
+  if(is.null(station_id)) {
+    station_id <- "station_id"
+    data[[station_id]] <- 1
+    created_station <- TRUE
+  } else {
+    created_station <- FALSE
   }
   
-  if (missing(elements)) {
-    stop('argument "elements" is missing, with no default')
-  }
-  
-  # stack data
-  data.stack <- data %>%
-    tidyr::pivot_longer(cols = c({{ elements }}),
-                        names_to = "Element",
+  data_longer <- data %>% 
+    tidyr::pivot_longer(cols = tidyselect::all_of(elements), 
+                        names_to = "element",
                         values_to = "value")
-  
-  # sort start/end times
-  
-  # set start date_time
   if (start) {
-    data.stack <- data.stack %>%
-      dplyr::group_by({{ stations }}, .data$Element) %>%
-      dplyr::mutate(start = ({{ date_time }})[which.min(is.na( .data$value ))])
+    data_longer <- data_longer %>%
+      dplyr::group_by(.data[[station_id]], .data$element) %>%
+      dplyr::mutate(start = (data[[date_time]])[which.min(is.na(.data$value))])
     
   } else {
-    data.stack <- data.stack %>%
-      dplyr::group_by({{ stations }}) %>%
-      dplyr::mutate(start = dplyr::first( {{ date_time }} ))
+    data_longer <- data_longer %>%
+      dplyr::group_by(.data[[station_id]]) %>%
+      dplyr::mutate(start = dplyr::first(data[[date_time]]))
   }
   
-  # set end date_time
   if (end) {
-    data.stack <- data.stack %>%
-      dplyr::group_by({{ stations }}, .data$Element ) %>%
-      dplyr::mutate(end = ({{ date_time }} )[dplyr::last(which(!is.na( .data$value )))])
+    data_longer <- data_longer %>%
+      dplyr::group_by(.data[[station_id]], .data$element) %>%
+      dplyr::mutate(end = (data[[date_time]])[dplyr::last(which(!is.na(.data$value)))])
   } else {
-    data.stack <- data.stack %>%
-      dplyr::group_by({{ stations }} ) %>%
-      dplyr::mutate(end = dplyr::last({{ date_time }}))
+    data_longer <- data_longer %>%
+      dplyr::group_by(.data[[station_id]]) %>%
+      dplyr::mutate(end = dplyr::last(data[[date_time]]))
   }
   
   # number and percentage missing
-  summary.data <- data.stack %>%
-    dplyr::group_by({{ stations }}, .data$Element) %>%
-    dplyr::filter(({{ date_time }}) >= start & ({{ date_time }}) <= end) %>%
-    dplyr::summarise(From = dplyr::first(start),
-                     To = dplyr::last(end),
-                     Missing = sum(is.na(.data$value)),
+  summary.data <- data_longer %>%
+    dplyr::group_by(.data[[station_id]], .data$element) %>%
+    dplyr::filter((.data[[date_time]]) >= start & (.data[[date_time]]) <= end) %>%
+    dplyr::summarise(from = dplyr::first(start),
+                     to = dplyr::last(end),
+                     missing = sum(is.na(.data$value)),
                      `%` = round(sum(is.na(.data$value))/dplyr::n()*100, 1))
-  
   # complete years
-  complete.years <- data.stack %>%
-    dplyr::group_by({{ stations }}) %>%
-    dplyr::filter(({{ date_time }}) >= start & ({{ date_time }}) <= end) %>%
-    dplyr::group_by(lubridate::year({{ date_time }}), {{ stations }}, .data$Element) %>%
+  complete.years <- data_longer %>%
+    dplyr::group_by(.data[[station_id]]) %>%
+    dplyr::filter((.data[[date_time]]) >= start & (.data[[date_time]]) <= end) %>%
+    dplyr::group_by(lubridate::year(.data[[date_time]]), .data[[station_id]], .data$element) %>%
     dplyr::summarise(count = sum(is.na(.data$value)))
   complete.years <- complete.years %>%
-    dplyr::group_by({{ stations }}, .data$Element) %>%
-    dplyr::summarise(Full_Years = sum(.data$count == 0))
-  
-  
+    dplyr::group_by(.data[[station_id]], .data$element) %>%
+    dplyr::summarise(full_years = sum(.data$count == 0))
+
   # bind together
   summary.data <- merge(summary.data, complete.years)
   
-  if (missing(stations)) {
-    summary.data$stations <- NULL
+  if(created_station) {
+    summary.data <- summary.data %>% dplyr::select(-.data[[station_id]])
   }
   
   return(summary.data)
-}  
+}
