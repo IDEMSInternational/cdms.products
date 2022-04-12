a9afc0ee36dd41fe5d0d20f9feeeac5a18acb8c3
Haward Ketoyo <hketoyo@gmail.com>
2022-04-12 07:16:23 +0300

    Updated code
    Updated tests for export_geoclim, export_geoclim_dekad, and export_geoclim_pentad.
    Removed test for export_geoclim_month.

--- tests/testthat/test-export_geoclim.R
+++ tests/testthat/test-export_geoclim.R
@@ -1,1 +1,81 @@
+library(cdms.products)
+library(dplyr)
+library(readr)
 
+niger51 <- daily_niger %>%
+  dplyr::filter(year == 1951)
+
+niger_dekad <- niger51 %>%
+  mutate(dekad = dekad(date)) %>%
+  group_by(station_name, year, dekad) %>%
+  summarise(rain = sum(rain))
+
+niger_pentad <- niger51 %>%
+  mutate(pentad = pentad(date)) %>%
+  group_by(station_name, year, pentad) %>%
+  summarise(rain = sum(rain))
+
+df_dekad <- left_join(niger_dekad, stations_niger, by = "station_name")
+
+df_pentad <- left_join(niger_pentad, stations_niger, by = "station_name")
+
+x_dekad <- prepare_geoclim(data = df_dekad, year = "year", type_col = "dekad",
+                            element = "rain", station_id = "id", latitude = "lat",
+                            longitude = "long", type = "dekad")
+y_dekad <- export_geoclim(data = df_dekad, year = "year", type_col = "dekad",
+                           element = "rain", station_id = "id", latitude = "lat",
+                           longitude = "long", type = "dekad", metadata = NULL, 
+                           join_by = NULL, add_cols = NULL,
+                           file_path = paste0("GEOCLIM-", "rain", ".csv"))
+y_dekad <- readr::read_csv(paste0("GEOCLIM-", "rain", ".csv")) 
+
+test_that("export_geoclim gives correct values", {
+  expect_equal(x_dekad, y_dekad)
+})
+
+x_dekad <- prepare_geoclim(data = df_dekad, year = "year", type_col = "dekad",
+                            element = "rain", station_id = "id", latitude = "lat",
+                            longitude = "long", type = "dekad", 
+                            metadata = stations_niger, join_by = "station_name")
+y_dekad <- export_geoclim(data = df_dekad, year = "year", type_col = "dekad",
+                           element = "rain", station_id = "id", latitude = "lat",
+                           longitude = "long", type = "dekad", 
+                           metadata = stations_niger, join_by = "station_name",
+                           add_cols = NULL,
+                           file_path = paste0("GEOCLIM-",  "rain", ".csv"))
+y_dekad <- readr::read_csv(paste0("GEOCLIM-",  "rain", ".csv")) 
+
+test_that("export_cdt gives correct values", {
+  expect_equal(x_dekad, y_dekad)
+})
+
+
+x_pentad <- prepare_geoclim(data = df_pentad, year = "year", type_col = "pentad",
+                            element = "rain", station_id = "id", latitude = "lat",
+                            longitude = "long", type = "pentad")
+y_pentad <- export_geoclim(data = df_pentad, year = "year", type_col = "pentad",
+                           element = "rain", station_id = "id", latitude = "lat",
+                           longitude = "long", type = "pentad", metadata = NULL, 
+                           join_by = NULL, add_cols = NULL,
+                           file_path = paste0("GEOCLIM-",  "rain", ".csv"))
+y_pentad <- readr::read_csv(paste0("GEOCLIM-",  "rain", ".csv")) 
+
+test_that("export_geoclim gives correct values", {
+  expect_equal(x_pentad, y_pentad)
+})
+
+x_pentad <- prepare_geoclim(data = df_pentad, year = "year", type_col = "pentad",
+                            element = "rain", station_id = "id", latitude = "lat",
+                            longitude = "long", type = "pentad", 
+                            metadata = stations_niger, join_by = "station_name")
+y_pentad <- export_geoclim(data = df_pentad, year = "year", type_col = "pentad",
+                           element = "rain", station_id = "id", latitude = "lat",
+                           longitude = "long", type = "pentad", 
+                           metadata = stations_niger, join_by = "station_name",
+                           add_cols = NULL,
+                           file_path = paste0("GEOCLIM-",  "rain", ".csv"))
+y_pentad <- readr::read_csv(paste0("GEOCLIM-",  "rain", ".csv")) 
+
+test_that("export_cdt gives correct values", {
+  expect_equal(x_pentad, y_pentad)
+})
--- tests/testthat/test-export_geoclim_dekad.R
+++ tests/testthat/test-export_geoclim_dekad.R
@@ -1,3 +1,45 @@
+library(cdms.products)
+library(dplyr)
+library(readr)
 
+niger51 <- daily_niger %>%
+  dplyr::filter(year == 1951)
 
+niger_dekad <- niger51 %>%
+  mutate(dekad = dekad(date)) %>%
+  group_by(station_name, year, dekad) %>%
+  summarise(rain = sum(rain))
 
+df_dekad <- left_join(niger_dekad, stations_niger, by = "station_name")
+
+x_dekad <- prepare_geoclim(data = df_dekad, year = "year", type_col = "dekad",
+                           element = "rain", station_id = "id", latitude = "lat",
+                           longitude = "long", type = "dekad")
+
+y_dekad <- export_geoclim_dekad(data = df_dekad, dekad = "dekad", year = "year",
+                          element = "rain", station_id = "id", latitude = "lat",
+                          longitude = "long", metadata = NULL, 
+                          join_by = NULL, add_cols = NULL,
+                          file_path = paste0("GEOCLIM-",  "rain", ".csv"))
+y_dekad <- readr::read_csv(paste0("GEOCLIM-",  "rain", ".csv")) 
+
+test_that("export_geoclim_dekad gives correct values", {
+  expect_equal(x_dekad, y_dekad)
+})
+
+x_dekad <- prepare_geoclim(data = df_dekad, year = "year", type_col = "dekad",
+                           element = "rain", station_id = "id", latitude = "lat",
+                           longitude = "long", type = "dekad", 
+                           metadata = stations_niger, join_by = "station_name")
+
+y_dekad <- export_geoclim_dekad(data = df_dekad, year = "year", dekad = "dekad",
+                          element = "rain", station_id = "id", latitude = "lat",
+                          longitude = "long", type = "dekad", 
+                          metadata = stations_niger, join_by = "station_name",
+                          add_cols = NULL,
+                          file_path = paste0("GEOCLIM-",  "rain", ".csv"))
+y_dekad <- readr::read_csv(paste0("GEOCLIM-",  "rain", ".csv")) 
+
+test_that("export_geoclim_dekad gives correct values", {
+  expect_equal(x_dekad, y_dekad)
+})
