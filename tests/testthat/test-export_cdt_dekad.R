b54f5695bf28ed79114afb73d4f45568c9735511
lilyclements <lily@idems.international>
2022-04-26 08:51:22 +0100

    Update test-export_cdt_dekad.R

--- tests/testthat/test-export_cdt_dekad.R
+++ tests/testthat/test-export_cdt_dekad.R
@@ -16,11 +16,12 @@ x_dekad <- prepare_cdt(data = dekad_summary_data, date_time = "date", year = "ye
                        latitude = "lat", longitude = "long", altitude = "alt", type = "dekad")
 
 # create functions dekad output
+element <- "sum"
 y_dekad <- export_cdt_dekad(data = dekad_summary_data, station = "station_name", 
-                      element = "sum", latitude = "lat", longitude = "long", 
-                      altitude = "alt", type =  "dekad", date_time = "date", 
-                      metadata = stations_niger, 
-                      file_path = paste0("CDT-", element, ".csv"))
+                            element = element, latitude = "lat", longitude = "long", 
+                            altitude = "alt", type =  "dekad", date_time = "date", 
+                            metadata = stations_niger, 
+                            file_path = paste0("CDT-", element, ".csv"))
 y_dekad <- readr::read_csv(paste0("CDT-", element, ".csv"))
 
 test_that("export_cdt gives correct values", {
@@ -29,7 +30,7 @@ test_that("export_cdt gives correct values", {
 
 test_that("export_cdt_dekad returns an error when conditions are not met", {
   mandatory_inputs <- list(data = dekad_summary_data, station = "station_name", 
-                           element = "sum", latitude = "lat", longitude = "long", 
+                           element = element, latitude = "lat", longitude = "long", 
                            altitude = "alt", date_time = "date", 
                            metadata = stations_niger)
   
@@ -46,7 +47,7 @@ test_that("export_cdt_dekad returns an error when conditions are not met", {
 
 test_that("export_cdt_dekad returns an error when numbers outside valid ranges are supplied", {
   entries_outside_valid_ranges <- data.frame(year = character(), station_name = integer(), 
-                                dekad_date = character(), date =  numeric(), sum = character())
+                                             dekad_date = character(), date =  numeric(), sum = character())
   expect_error(export_cdt_dekad(data = entries_outside_valid_ranges, station = "station_name", 
                                 element = "sum", latitude = "lat", longitude = "long", 
                                 altitude = "alt", type =  "dekad", date_time = "date", 
@@ -56,14 +57,14 @@ test_that("export_cdt_dekad returns an error when numbers outside valid ranges a
 
 test_that("export_cdt_dekad  does not return an error when an empty dataframe is supplied", {
   empty_metadata <- data.frame(station_name = character(), id = integer(), 
-                              lat = numeric(), long = numeric(), alt = integer(), daily = logical())
+                               lat = numeric(), long = numeric(), alt = integer(), daily = logical())
   empty_dataframe <- data.frame(station_name = character(), year = integer(), 
-                               dekad_date = numeric(), date =  as.Date(integer(0)), sum = numeric())
+                                dekad_date = numeric(), date =  as.Date(integer(0)), sum = numeric())
   expect_error(export_cdt_dekad(data = empty_dataframe, station = "station_name", 
-                               element = "sum", latitude = "lat", longitude = "long", 
-                               altitude = "alt", type =  "dekad", date_time = "date", 
-                               metadata = empty_metadata, 
-                               file_path = paste0("CDT-", element, ".csv")),NA)
+                                element = "sum", latitude = "lat", longitude = "long", 
+                                altitude = "alt", type =  "dekad", date_time = "date", 
+                                metadata = empty_metadata, 
+                                file_path = paste0("CDT-", element, ".csv")),NA)
   
 })
 
